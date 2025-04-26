717926a329a06afff4ea10cbc8130d45255b62a7
Rania <151019884+ranitherat@users.noreply.github.com>
2025-04-26 17:19:05 +0800

    Update database.js

--- database.js
+++ database.js
@@ -15,7 +15,7 @@ document.getElementById("myForm").addEventListener("submit", function (e) {
     console.log("Prepared data:", data); // Check if it's properly formatted
 
     // ✅ 1. Send to Google Sheets
-    fetch("https://script.google.com/macros/s/AKfycbxOZTGtsO8oXKy_0GOqlUQqO0OECxu9K7xW3qWLyM0Xu82MKbKb7EJqUs1XU-aqPqnL4A/exec", {
+    fetch("https://script.google.com/macros/s/AKfycbwqM40DBQm-YIDQQz6bULR_PfJbFgULBmhrtmnLeGNb8EV9b9mNRKLcCXx_elsj4fDf3Q/exec", {
         method: "POST",
         mode: "no-cors",
         headers: {
